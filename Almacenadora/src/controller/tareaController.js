b7d3f1d448f264a65050945c81f619692a25725e
Castro._2004 <107076105+jcastro2021416@users.noreply.github.com>
2023-04-21 09:03:35 -0600

    create-task
    Se agrego create-task tarea en la el archivo tareaController.js

--- Almacenadora/src/controller/tareaController.js
+++ Almacenadora/src/controller/tareaController.js
@@ -1,7 +1,36 @@
 'use strict'
 
+const Task = require("../model/taskModel")
 const User = require("../model/userModel")
 
-//------------------------------------create 
+//------------------------------------create tarea-------------------------------------
 
+const createTask = async(req, res) => {
+    const {nameTask, description, startDate, dateFinish} = req.body
+    try{
+        const task = await Task.findOne({nameTask});
+        if(task){
+            return res.status(410).json({
+                msg: "Ya existe una tarea registrada con este nombre",
+                ok: false,
+                tarea: task,
+            });
+        }
+        const newTask = await Task.create({nameTask, description, startDate, dateFinish});
 
+        User.task = newTask._id
+        await User.task.save()
+
+        res.status(200).send({
+            ok: true,
+            msg: `${nameTask} se creo de forma correcta`,
+            tarea: newTask
+        })
+    }catch(err){
+        console.log(err)
+    }
+}
+
+module.exports = {
+    createTask
+}
